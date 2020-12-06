91a3b3e637c64bcc40336d4ec691e9eb78b1bf2b
DipokalLab <gipocation@gmail.com>
2020-12-06 23:24:15 +0900

    Base01
    Base01

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const main = require('./run/main');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('pub'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
+app.get('/', main.load_index);
+app.post('/init', main.load_init);
+
+
+server.listen(5004);
---
+++ run/main.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const config = require('../config/index.json');
+
+exports.load_index = function (req, res) {
+    res.render("index");
+}
+
+exports.load_init = function (req, res) {
+    res.json({
+        title:config.sitetitle
+    });
+}
+
+/*
+exports.load_login = function (req, res) {
+    res.render("auth/login", {
+        title:config.sitetitle+" | "+config.loginsubtitle
+    });
+}
+
+exports.load_join = function (req, res) {
+    res.render("auth/join", {
+        title:config.sitetitle+" | "+config.joinsubtitle
+    });
+}
+*/
